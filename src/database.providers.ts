93a20f58a43d27feb6f1e9ead965318577203f0d
Victor <victor.henriqueoff@gmail.com>
2025-03-21 16:19:39 -0300

    feat: adding graphql in app

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -5,11 +5,7 @@ export const databaseProviders = [
     provide: "DATA_SOURCE",
     useFactory: async () => {
       const dataSource = new DataSource({
-        type: "postgres",
-        host: "localhost",
-        port: 5432,
-        username: "root",
-        password: "root",
+        type: "sqlite",
         database: "test",
         entities: [__dirname + "/../**/*.entity{.ts,.js}"],
         synchronize: true,
